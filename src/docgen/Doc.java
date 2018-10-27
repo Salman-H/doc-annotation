7e4a92990831668bcef94ceb9fbe38d173480a5c
Salman Hashmi <shash24@hotmail.ca>
2018-10-27 02:05:32 -0400

    feat: Specify classes and methods as annotation targets

--- src/docgen/Doc.java
+++ src/docgen/Doc.java
@@ -1,8 +1,11 @@
 package docgen;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Doc {
 }
